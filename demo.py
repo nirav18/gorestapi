db30231464f2460cccd327853b3575b7da7a6194
nirav18 <niravthakkar9838@gmail.com>
2024-11-14 13:14:42 -0600

    Create demo.py
    demo.py

---
+++ demo.py
@@ -0,0 +1,62 @@
+import datetime as dt
+
+from nautobot.apps.jobs import Job
+from nautobot.core.celery import register_jobs
+from nautobot.users.models import Token
+from nautobot.extras.models.secrets import Secret
+from nautobot.extras.secrets.exceptions import SecretError
+
+name = "Token Lifecycle Job"
+
+
+
+
+class TokenLifecycle(Job):
+
+    """
+        Token Lifecycle job
+    """
+
+    class Meta:
+        " Metadata needed to implement the backbone site design. "
+
+        name = "Token Lifecycle"
+        commit_default = False
+        has_sensitive_variables = False
+    
+    def token_exception_list(self, t):
+        try:
+            secret = Secret.objects.get(name="secret1")   # env variable for token exception
+            token_exception_list = secret.get_value()
+            if t.key in token_exception_list:
+                return True
+        except SecretError as exc:
+            self.logger.warning(exc)
+            self.logger.info("checking user tokens for admin user")
+            if t.user.username in ["admin"]:
+                return True
+    
+        
+
+    def get_tokens(self):
+        tokens = Token.objects.all()
+        token_list = []
+        is_valid = True
+        for t in tokens:
+            if t.is_expired:
+                is_valid = False
+            if t.created.day < 15 and not self.token_exception(t):
+                is_valid = False
+            if not is_valid:
+                token_list.append({"t_user": t.user.username, "t_created": t.created.strftime("%Y-%m-%d %H:%M:%S UTC"), "t_id": t.id, "t_is_expired": t.is_expired, "t_key": t.key })
+                t.delete()
+        return token_list
+
+    def run(self, **data):
+        resp = self.get_tokens()
+        if len(resp) > 0:
+            return (f"deleted objects: {resp}")
+        else:
+            return ("no objects found to delete")
+
+register_jobs(TokenLifecycle)
